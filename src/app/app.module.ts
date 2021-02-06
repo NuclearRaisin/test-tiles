d2a51db548d905014512b1449b50d299742857d3
matejhynek <matej.hynek@blogic.cz>
2021-02-06 09:26:20 +0100

    #0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TileItemComponent } from './tile-item/tile-item.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TileItemComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tile-item/tile-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tile-item',
+  templateUrl: './tile-item.component.html',
+  styleUrls: ['./tile-item.component.css']
+})
+export class TileItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
